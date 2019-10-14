abd89a3fc928ae74339f37b2f93fceb577d0783c
Olsoo <37656328+Olsoo@users.noreply.github.com>
2019-10-14 13:19:38 +0200

    eventsystem

---
+++ Devour/Devour/Assets/Scripts/Events/Event.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Event<T> where T : Event<T> {
+
+    public string Description;
+
+    private bool hasFired;
+    public delegate void EventListener(T info);
+    private static event EventListener listeners;
+
+    public static void RegisterListener(EventListener listener) {
+        listeners += listener;
+    }
+
+    public static void UnRegisterListener(EventListener listener) {
+        listeners -= listener;
+    }
+
+    public void FireEvent() {
+        if (hasFired) {
+            throw new System.Exception("This event has already fired, to prevent infinite loops you can't refire an event");
+        }
+        hasFired = true;
+        listeners?.Invoke(this as T);
+    }
+}
+
