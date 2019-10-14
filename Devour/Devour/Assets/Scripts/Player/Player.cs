e3944196f401ec5eb49bcf9ad1db74c95a360bee
Olsoo <37656328+Olsoo@users.noreply.github.com>
2019-10-14 21:04:12 +0200

    Update Player.cs

--- Devour/Devour/Assets/Scripts/Player/Player.cs
+++ Devour/Devour/Assets/Scripts/Player/Player.cs
@@ -5,36 +5,46 @@ using UnityEngine;
 public class Player : StateMachine {
 
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float wallSlideSpeed;
     [SerializeField] private float jumpForce;
     [SerializeField] private int extraJumps;
+    private float variableJumpHeight = 0.4f;
     private float xScale;
     private int extraJumping;
     private float XInput;
     private float YInput;
-    [SerializeField] private float fakeExtraGravity;
+    
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private float wallCheckDistance;
+    private float wallCheckValue;
 
     [SerializeField] private bool isGrounded;
+    [SerializeField] private bool isTouchingWall;
+    [SerializeField] private bool isWallSliding;
 
     private bool isFacingRight;
     private Rigidbody2D rb2D;
     [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform wallCheck;
     [SerializeField] private LayerMask whatIsGround;
 
     void Start() {
         rb2D = GetComponent<Rigidbody2D>();
         isGrounded = false;
         xScale = transform.localScale.x;
+        wallCheckValue = wallCheckDistance;
     }
     
     protected override void FixedUpdate() {
-        FakeGravity();
-        rb2D.velocity = new Vector2(XInput * movementSpeed, rb2D.velocity.y + fakeExtraGravity);//
+        FacingDirection();
+        
+        MovePlayer();
     }
 
     protected override void Update() {
+        CollisionCheck();
         GetInput();
-        FacingDirection();
-        JumpCheck();
+        JumpCheck();        
     }
 
     private void GetInput() {
@@ -42,11 +52,22 @@ public class Player : StateMachine {
     }
 
 
+    private void MovePlayer() {
+        rb2D.velocity = new Vector2(XInput * movementSpeed, rb2D.velocity.y );
+        if (isWallSliding) {
+            if(rb2D.velocity.y < -wallSlideSpeed) {
+                rb2D.velocity = new Vector2(rb2D.velocity.x, -wallSlideSpeed);
+            }
+        }
+    }
+
     public void FacingDirection() {
         if (XInput < 0) {
             Flip(-xScale);
+            wallCheckValue = -wallCheckDistance;
         } else if (XInput > 0) {
             Flip(xScale);
+            wallCheckValue = wallCheckDistance;
         }
     }
     private void Flip(float direction) {
@@ -55,47 +76,54 @@ public class Player : StateMachine {
         transform.localScale = myScale;
     }
 
-    private void FakeGravity() {
-        if (!isGrounded) {
-            fakeExtraGravity -= 2 * Time.deltaTime;
-        } else {
-            fakeExtraGravity = 0;
-        }
-        if (fakeExtraGravity <= -0.5f) {
-            fakeExtraGravity = -0.5f;
-        }
+    private void CollisionCheck() {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckDistance, whatIsGround);
+        isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckValue, whatIsGround);
+        WallSlideCheck();
     }
 
-    private void CollisionCheck() {
-
+    private void WallSlideCheck() {
+        if(isTouchingWall && !isGrounded && rb2D.velocity.y < 0) {
+            isWallSliding = true;
+        } else {
+            isWallSliding = false;
+        }
     }
 
     private void JumpCheck() {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.15f, whatIsGround);
-        if (isGrounded) {
+        
+        if (isGrounded || isWallSliding) {
             extraJumping = extraJumps;
+        }
+        if (isWallSliding && Input.GetKeyDown(KeyCode.Space)) {
+            Jump(0);
+            return;
         }
         if (isGrounded && Input.GetKeyDown(KeyCode.Space)) {
             Jump(0);
+            return;
         }
         if (!isGrounded && extraJumping > 0 && Input.GetKeyDown(KeyCode.Space)) {
             extraJumping--;
             rb2D.velocity = new Vector2(0, 0);
-            fakeExtraGravity = 0;
-            Jump(5);
+            Jump(0);
+        }
+        if (Input.GetKeyUp(KeyCode.Space) && rb2D.velocity.y > 0) {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, rb2D.velocity.y * variableJumpHeight);
         }
     }
     
-    private void Jump(float extra) {
-        rb2D.velocity = Vector2.up * (jumpForce + extra);
+    private void Jump(float walldistance) {
+        rb2D.velocity = new Vector2((5 * walldistance), (1 * jumpForce));
     }
 
     public void PlayerLog(string message) {
         Debug.Log("PLAYER: " + message);
     }
 
-    private void OnDrawGizmos() {
-        Gizmos.DrawWireSphere(groundCheck.position, 0.15f);
-    }
+    //private void OnDrawGizmos() {
+    //    Gizmos.DrawWireSphere(groundCheck.position, groundCheckDistance);
+    //    Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckValue, wallCheck.position.y, wallCheck.position.z));
+    //}
 
 }
