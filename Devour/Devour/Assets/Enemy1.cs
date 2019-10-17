1caf4606e6c799099bc51adc742de4b25b070a17
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2019-10-17 14:53:05 +0200

    enemy 1 och 2

---
+++ Devour/Devour/Assets/Enemy1.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private Transform enemyGFX;
+    [SerializeField] private float speed = 200f;
+    [SerializeField] private float distanceBeforeTurning = 1f;
+
+    private Rigidbody2D rb;
+    
+    private Vector2 direction;
+    private Vector2 force;
+    private bool movingRight = true;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (movingRight)
+        {
+            direction = new Vector2(1f, 0f);
+        }
+        else if (!movingRight)
+        {
+            direction = new Vector2(-1f, 0f);
+        }
+
+        force = direction.normalized * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        if (rb.velocity.x >= 0.01f)
+        {
+            enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (rb.velocity.x <= -0.01f)
+        {
+            enemyGFX.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        RaycastHit2D obstructed = Physics2D.Raycast(rb.position, direction, distanceBeforeTurning);
+        if (obstructed.collider == true)
+        {
+            movingRight = !movingRight;
+        }
+    }
+
+}
---
+++ Devour/Devour/Assets/Enemy2.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Enemy2 : MonoBehaviour
+{
+
+    [SerializeField] private Transform target;
+    [SerializeField] private float speed = 200f;
+    [SerializeField] private float nextWaypointDistance = 3f;
+    [SerializeField] private Transform enemyGFX;
+    [SerializeField] private float attackDistance = 25f;
+
+    private Path path;
+    private int currentWaypoint = 0;
+    private bool reachedEndOfPath = false;
+    private bool isWithinAttackDistance = false;
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    private Vector2 direction;
+    private Vector2 force;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("UpdatePath", 0f, .5f);
+        target = FindObjectOfType<Player>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!isWithinAttackDistance)
+        {
+            if (Vector2.Distance(rb.position, target.position) <= attackDistance)
+            {
+                isWithinAttackDistance = true;
+            }
+        }
+
+        if (isWithinAttackDistance)
+        {
+            if (path == null)
+            {
+                return;
+            }
+
+            if (currentWaypoint >= path.vectorPath.Count)
+            {
+                reachedEndOfPath = true;
+                return;
+            }
+            else
+            {
+                reachedEndOfPath = false;
+            }
+
+            direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+            force = direction * speed * Time.deltaTime;
+
+            rb.AddForce(force);
+
+            float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+            if (distance < nextWaypointDistance)
+            {
+                currentWaypoint++;
+            }
+
+            if (rb.velocity.x >= 0.01f)
+            {
+                enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (rb.velocity.x <= -0.01f)
+            {
+                enemyGFX.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+}
