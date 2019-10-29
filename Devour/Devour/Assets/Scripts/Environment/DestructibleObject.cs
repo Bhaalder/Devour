f7bde276c83b451107234f1c5670ce9f755b6948
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2019-10-29 14:01:09 +0100

    destructible (simple)
    todo:
    add support for sound
    add support for animation/image change
    add support for currently unknown systems

---
+++ Devour/Devour/Assets/Scripts/Environment/DestructibleObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour
+{
+    [SerializeField] private float health = 100;
+
+    private BoxCollider2D boxCollider2D;
+
+    void Start()
+    {
+        PlayerAttackEvent.RegisterListener(TakeDamage);
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void TakeDamage(PlayerAttackEvent attackEvent)
+    {
+        if (attackEvent.attackCollider.bounds.Intersects(boxCollider2D.bounds))
+        {
+            health -= attackEvent.damage;
+            if (health <= 0)
+            {
+                DestroyObject();
+            }
+        }
+    }
+
+    private void DestroyObject()
+    {
+        PlayerAttackEvent.UnRegisterListener(TakeDamage);
+        Destroy(gameObject);
+    }
+}
