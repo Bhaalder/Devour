6f52f593d6349fbb069ef8e450cdeb9ccbf9a9d8
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2019-10-29 20:59:54 +0100

    destructible
    added more control over features
    added wait period before destruction (perhaps to wait for crumble animation or something similar)

--- Devour/Devour/Assets/Scripts/Environment/DestructibleObject.cs
+++ Devour/Devour/Assets/Scripts/Environment/DestructibleObject.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class DestructibleObject : MonoBehaviour
 {
     [SerializeField] private float health = 100;
-    [SerializeField] private bool isUsingStages;
+    [SerializeField] private bool isUsingStageDamagedHealth;
+    [SerializeField] private bool isUsingStageHalfHealth;
+    [SerializeField] private bool isUsingDestroyCooldown;
     [SerializeField] GameObject fullHealth;
     [SerializeField] GameObject damagedHealth;
     [SerializeField] GameObject halfHealth;
+    [SerializeField] float destroyCooldown = 2f;
 
     private BoxCollider2D boxCollider2D;
+
+    private bool isOnCooldown;
+
     private float originalHealth;
+    private float currentCooldown;
 
     void Start()
     {
         PlayerAttackEvent.RegisterListener(TakeDamage);
         boxCollider2D = GetComponent<BoxCollider2D>();
         originalHealth = health;
+        currentCooldown = destroyCooldown;
+        isOnCooldown = false;
+    }
+
+    private void Update()
+    {
+        if (isOnCooldown)
+        {
+            DestroyCooldown();
+        }
     }
 
     private void TakeDamage(PlayerAttackEvent attackEvent)
@@ -25,13 +42,13 @@ public class DestructibleObject : MonoBehaviour
         if (attackEvent.attackCollider.bounds.Intersects(boxCollider2D.bounds))
         {
             health -= attackEvent.damage;
-            if (health < originalHealth && health > originalHealth/2 && isUsingStages)
+            if (health < originalHealth && health > originalHealth/2 && isUsingStageDamagedHealth)
             {
                 fullHealth.SetActive(false);
                 damagedHealth.SetActive(true);
                 halfHealth.SetActive(false);
             }
-            if (health <= originalHealth / 2 && isUsingStages)
+            if (health <= originalHealth / 2 && isUsingStageHalfHealth)
             {
                 fullHealth.SetActive(false);
                 damagedHealth.SetActive(false);
@@ -39,15 +56,36 @@ public class DestructibleObject : MonoBehaviour
             }
             if (health <= 0)
             {
-                DestroyObject();
+                if (isUsingDestroyCooldown)
+                {
+                    isOnCooldown = true;
+                    //start destroy animation?
+                }
+                else
+                {
+                    DestroyObject();
+                }
             }
         }
+    }
+
+    private void DestroyCooldown()
+    {
+        currentCooldown -= Time.deltaTime;
+
+        if (currentCooldown > 0)
+        {
+            return;
+        }
+
+        DestroyObject();
     }
 
     private void DestroyObject()
     {
         Destroy(gameObject);
     }
+
     private void OnDestroy()
     {
         PlayerAttackEvent.UnRegisterListener(TakeDamage);
