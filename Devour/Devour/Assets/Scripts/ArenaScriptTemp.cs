8dfd71487d9907d24b47c7fd24df8b6ae991f120
teoalagic <teoalagicenglund@hotmail.com>
2019-11-04 16:47:31 +0100

    Ändringar i scener. Arena område. Boss tweaks.

---
+++ Devour/Devour/Assets/Scripts/ArenaScriptTemp.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaScriptTemp : MonoBehaviour
+{
+    public GameObject lockedEnterWall;
+    public GameObject lockedExitWall;
+    public GameObject enemiesParent;
+    void Start()
+    {
+        lockedEnterWall.SetActive(false);
+        enemiesParent.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try {
+            if (enemiesParent.transform.childCount <= 0)
+            {
+                lockedExitWall.SetActive(false);
+                lockedEnterWall.SetActive(false);
+
+            }
+        }
+        catch(NullReferenceException e)
+        {
+            Debug.Log("Null Error");
+        }
+        
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(activateArena());
+        }
+    }
+
+    IEnumerator activateArena()
+    {
+        lockedEnterWall.SetActive(true);
+
+        yield return new WaitForSeconds(3);
+        
+        enemiesParent.SetActive(true);
+    }
+}
---
+++ Devour/Devour/Assets/Scripts/TempHiddenAreaScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempHiddenAreaScript : MonoBehaviour
+{
+
+    public GameObject theSprite;
+    public bool isPermanent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        if (collision.CompareTag("Player"))
+        {
+            theSprite.SetActive(false);
+            Debug.Log("Enter");
+        }
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Player") && !isPermanent)
+        {
+            theSprite.SetActive(true);
+            Debug.Log("Exit");
+        }
+        
+            
+    }
+}
