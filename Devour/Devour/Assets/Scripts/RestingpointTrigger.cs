a26d6c9fe5cfab59f77dee47d5f967647b35dbc7
Olsoo <37656328+Olsoo@users.noreply.github.com>
2019-11-07 21:47:39 +0100

    RestingpointTrigger

---
+++ Devour/Devour/Assets/Scripts/RestingpointTrigger.cs
@@ -0,0 +1,20 @@
+﻿//Author: Patrik Ahlgren
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestingpointTrigger : MonoBehaviour {
+    //Glöm ej att sätta ut spawnpoint vid rätt plats, det är dess child
+
+    private void Awake() {
+        Destroy(transform.GetChild(0).GetComponent<SpriteRenderer>());
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            GameController.Instance.RestingScene = SceneManager.GetActiveScene().name;
+            GameController.Instance.RestingCheckpoint = transform.GetChild(0).position;
+        }
+    }
+}
