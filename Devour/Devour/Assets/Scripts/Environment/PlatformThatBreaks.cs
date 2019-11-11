3a3da37edff5ed091ab2fc524646e02f2cdd4997
teoalagic <teoalagicenglund@hotmail.com>
2019-11-11 22:01:04 +0100

    Scene 8 o 9 :cat:

---
+++ Devour/Devour/Assets/Scripts/Environment/PlatformThatBreaks.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformThatBreaks : MonoBehaviour
+{
+    public bool triggerFloor = false;
+    public bool timerStart = false;
+
+    [SerializeField] float aTimer = 0.0f;
+    public GameObject thePlatform;
+
+    public GameObject particles;
+
+    private void Update()
+    {
+        if (triggerFloor == true)
+        {
+            TriggerFloor();
+            triggerFloor = false;
+        }
+        if (timerStart)
+        {
+            aTimer += Time.deltaTime;
+        }
+        if(aTimer >= 3.0f)
+        {
+            thePlatform.SetActive(true);
+            timerStart = false;
+            aTimer = 0;
+        }
+    }
+
+    void TriggerFloor()
+    {
+        GameObject instantiatedParticle = Instantiate(particles, null);
+        instantiatedParticle.transform.position = transform.position;
+        //Animation här
+        StartCoroutine(PlatformBreak());
+        //Destroy(this.gameObject);
+        Debug.Log("triggered");
+        
+    }
+
+    //Trigger the object's action when the player enters the trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player" && !triggerFloor)
+        {
+            triggerFloor = true;
+        }
+    }
+    IEnumerator PlatformBreak()
+    {
+        Debug.Log("Ienumerator");
+        
+        yield return new WaitForSeconds(3);
+        thePlatform.SetActive(false);
+        timerStart = true;
+
+    }
+
+}
