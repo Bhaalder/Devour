f8ba07f04842844adee635acfe6f5290d33bd644
Olsoo <37656328+Olsoo@users.noreply.github.com>
2019-11-15 00:41:53 +0100

    Sound

---
+++ Devour/Devour/Assets/Scripts/Audio/Music.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour{//DENNA ÄR TEMPORÄR, FIXA SOUNDMANAGER
+
+    [SerializeField] private string soundName;
+    [SerializeField] private SoundType soundType;
+
+    private void Start() {
+        AudioPlaySoundEvent ambience = new AudioPlaySoundEvent {
+            name = soundName,
+            soundType = soundType
+        };
+        ambience.FireEvent();
+    }
+
+}
