1119a86f9220bb8b16f0a0dcacd462b09954e49a
Olsoo <37656328+Olsoo@users.noreply.github.com>
2019-11-25 21:30:36 +0100

    Update OneTimeTipTrigger.cs

--- Devour/Devour/Assets/Scripts/UI/TipTriggers/OneTimeTipTrigger.cs
+++ Devour/Devour/Assets/Scripts/UI/TipTriggers/OneTimeTipTrigger.cs
@@ -10,13 +10,9 @@ public class OneTimeTipTrigger : TipTrigger {
 
     private void Start() {
         if (GameController.Instance.OneTimeTips.ContainsKey(SceneManager.GetActiveScene().name)) {
-            foreach (KeyValuePair<string, List<int>> essence in GameController.Instance.OneTimeTips) {
-                if (essence.Key == SceneManager.GetActiveScene().name) {
-                    if (essence.Value.Contains(tipID)) {
-                        Destroy(gameObject);
-                        return;
-                    }
-                }
+            if (GameController.Instance.OneTimeTips[SceneManager.GetActiveScene().name].Contains(tipID)) {
+                Destroy(gameObject);
+                return;
             }
         }
     }
@@ -24,16 +20,12 @@ public class OneTimeTipTrigger : TipTrigger {
     protected override void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
             if (GameController.Instance.OneTimeTips.ContainsKey(SceneManager.GetActiveScene().name)) {
-                foreach (KeyValuePair<string, List<int>> tip in GameController.Instance.OneTimeTips) {
-                    if (tip.Key == SceneManager.GetActiveScene().name) {
-                        if (tip.Value.Contains(tipID)) {
-                            Debug.LogWarning("A tip with the same ID [" + tipID + "] has already been seen in this scene [" + SceneManager.GetActiveScene().name + "]");
-                            Destroy(gameObject);
-                            return;
-                        }
-                        tip.Value.Add(tipID);
-                    }
+                if (GameController.Instance.OneTimeTips[SceneManager.GetActiveScene().name].Contains(tipID)) {
+                    Debug.LogWarning("A tip with the same ID [" + tipID + "] has already been seen in this scene [" + SceneManager.GetActiveScene().name + "]");
+                    Destroy(gameObject);
+                    return;
                 }
+                GameController.Instance.OneTimeTips[SceneManager.GetActiveScene().name].Add(tipID);
             } else {
                 List<int> newTipList = new List<int> { tipID };
                 GameController.Instance.OneTimeTips.Add(SceneManager.GetActiveScene().name, newTipList);
