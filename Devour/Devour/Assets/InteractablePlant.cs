9d5eea1a0201932cc37b305e3d7c2d937b464dcb
teoalagic <teoalagicenglund@hotmail.com>
2019-12-11 23:32:25 +0100

    InteractablePlant

---
+++ Devour/Devour/Assets/InteractablePlant.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractablePlant : MonoBehaviour
+{
+    private Animator anim;
+    private bool isTouching;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        isTouching = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (!isTouching)
+            {
+                anim.SetTrigger("Type1Trigger");
+                isTouching = true;
+            }
+        }
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isTouching = false;
+        }
+    }
+}
