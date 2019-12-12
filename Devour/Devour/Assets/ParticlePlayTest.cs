9f0d08be1e67f0baa8649496b185223fbfaf381e
teoalagic <teoalagicenglund@hotmail.com>
2019-12-12 15:06:12 +0100

    onödig slut partikel

---
+++ Devour/Devour/Assets/ParticlePlayTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePlayTest : MonoBehaviour
+{
+    public GameObject particleNorm, particleReverse;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Instantiate(particleNorm, transform.position, Quaternion.identity);
+        StartCoroutine(ReversePlay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void CalledUpon()
+    {
+        Instantiate(particleNorm);
+        StartCoroutine(ReversePlay());
+    }
+    IEnumerator ReversePlay()
+    {
+        yield return new WaitForSeconds(4);
+        Instantiate(particleReverse, transform.position, Quaternion.identity);
+        
+    }
+}
