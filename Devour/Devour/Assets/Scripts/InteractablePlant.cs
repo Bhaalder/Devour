443c30233cd749477a497f47f825a5127306c88f
Olsoo <37656328+Olsoo@users.noreply.github.com>
2019-12-12 13:09:06 +0100

    Update InteractablePlant.cs
    Nu svajar den beroende på var player är vänd

--- Devour/Devour/Assets/Scripts/InteractablePlant.cs
+++ Devour/Devour/Assets/Scripts/InteractablePlant.cs
@@ -2,39 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractablePlant : MonoBehaviour
-{
+public class InteractablePlant : MonoBehaviour {
+
     private Animator anim;
-    private bool isTouching;
-    void Start()
-    {
+
+    void Start() {
         anim = GetComponent<Animator>();
-        isTouching = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            if (!isTouching)
-            {
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            if (GameController.Instance.Player.FacingDirection == 1) {
                 anim.SetTrigger("Type1FromLeftTrigger");
-                isTouching = true;
+            } else if (GameController.Instance.Player.FacingDirection == -1) {
+                anim.SetTrigger("Type1FromRightTrigger");
             }
         }
-        
+
     }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            isTouching = false;
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+
         }
     }
 }
