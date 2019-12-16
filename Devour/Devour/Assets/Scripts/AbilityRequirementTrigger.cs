c48670d2c61562dd0a04bf7a15959ce55cb5389c
Olsoo <37656328+Olsoo@users.noreply.github.com>
2019-12-16 17:03:55 +0100

    abilityRequirement

---
+++ Devour/Devour/Assets/Scripts/AbilityRequirementTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityRequirementTrigger : MonoBehaviour{
+
+    [SerializeField] private PlayerAbility abilityRequired;
+
+    private void Start() {
+        if (!GameController.Instance.Player.HasAbility(abilityRequired)) {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
