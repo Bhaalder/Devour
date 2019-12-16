b117e21f55d77b9f905fd1b2e79ac803a279fed4
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2019-12-16 21:48:42 +0100

    PlantDestructionExample prefab
    - Check out PlantDestructionExample prefab under Prefab\Environment for how to use it
    
    Step1: Attach PlantDestruction.cs to parent
    Step 2: Attach particles to script, set amount of hits required to destroy it, and if the particles need an offset when spawning. Particle gamobject should preferably have DestroyTimer script on them.
    Step 3: Make sure the parent has a BoxCollider2D and that it has isTrigger.

---
+++ Devour/Devour/Assets/Scripts/Environment/PlantsDestruction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlantsDestruction : MonoBehaviour
+{
+    [SerializeField] private GameObject hitParticle;
+    [SerializeField] private GameObject destructionParticle;
+    [SerializeField] private Vector3 particleOffset;
+    [SerializeField] private int hitsToDestroy;
+
+    private BoxCollider2D boxCollider2D;
+    private int hits;
+
+    void Start()
+    {
+        PlayerAttackEvent.RegisterListener(TakeDamage);
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        if(hitsToDestroy < 1)
+        {
+            hitsToDestroy = 1;
+        }
+        hits = 0;
+    }
+
+    public void TakeDamage(PlayerAttackEvent attackEvent)
+    {
+        try
+        {
+            if (attackEvent.attackCollider.bounds.Intersects(boxCollider2D.bounds) && boxCollider2D.enabled == true)
+            {
+                hits++;
+                if (hits >= hitsToDestroy)
+                {
+                    Instantiate(destructionParticle, gameObject.transform.position + particleOffset, Quaternion.identity);
+                    Destroy(gameObject);
+                    return;
+                }
+                Instantiate(hitParticle, gameObject.transform.position + particleOffset, Quaternion.identity);
+            }
+        }
+        catch (NullReferenceException)
+        {
+            Debug.LogWarning("A missing reference in PlayerAttackEvent, check Log!");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        PlayerAttackEvent.UnRegisterListener(TakeDamage);
+    }
+}
