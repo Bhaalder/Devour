7f26b185a7b089620577117a215cfd11202fcb41
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2019-12-17 20:10:01 +0100

    blur settings

---
+++ Devour/Devour/Assets/Scripts/SaveLoadSystem/BlurSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class BlurSettings : MonoBehaviour
+{
+    private PostProcessVolume blur;
+
+    private 
+    // Start is called before the first frame update
+    void Start()
+    {
+        blur = GetComponent<PostProcessVolume>();
+        blur.enabled = DataStorage.Instance.SettingsData.DepthBlur;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
