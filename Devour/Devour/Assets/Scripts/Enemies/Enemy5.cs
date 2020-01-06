a24f07ffa81fc42ab0c3414207bc045831b2af5c
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2020-01-06 19:11:50 +0100

    enemy5

--- Devour/Devour/Assets/Scripts/Enemies/Enemy5.cs
+++ Devour/Devour/Assets/Scripts/Enemies/Enemy5.cs
@@ -46,8 +46,51 @@ public class Enemy5 : Enemy
                 enemyPosition = rb.position
             };
             ptde.FireEvent();
-            Stunned = true;
+
             Transition<Enemy5Hurt>();
+        }
+    }
+
+    public override void TakeDamage(PlayerAttackEvent attackEvent)
+    {
+        if (invulnerabilityTimer <= 0)
+        {
+            try
+            {
+                if (attackEvent.attackCollider.bounds.Intersects(boxCollider2D.bounds) && boxCollider2D.enabled == true)
+                {
+                    ChangeEnemyHealth(-attackEvent.damage);
+                    HurtSoundAndParticles();
+                    if (attackEvent.isMeleeAttack)
+                    {
+                        PlayerHealEvent phe = new PlayerHealEvent
+                        {
+                            isLifeLeech = true
+                        };
+                        if (attackEvent.player.HasAbility(PlayerAbility.VOIDMEND))
+                        {
+                            PlayerVoidChangeEvent voidEvent = new PlayerVoidChangeEvent
+                            {
+                                amount = attackEvent.player.MeleeVoidLeech
+                            };
+                            voidEvent.FireEvent();
+                        }
+                        phe.FireEvent();
+                        if (!attackEvent.player.IsGrounded && attackEvent.player.IsAttackingDown && attackEvent.isMeleeAttack)
+                        {
+                            attackEvent.player.ExtraJumpsLeft = attackEvent.player.ExtraJumps;
+                            attackEvent.player.DashesLeft = attackEvent.player.NumberOfDashes;
+                            attackEvent.player.Rb2D.velocity = new Vector2(attackEvent.player.Rb2D.velocity.x, 0);
+                            attackEvent.player.Rb2D.velocity = new Vector2(attackEvent.player.Rb2D.velocity.x, attackEvent.player.BounceForce);
+                            return;
+                        }
+                    }
+                }
+            }
+            catch (NullReferenceException)
+            {
+                Debug.LogWarning("A missing reference in PlayerAttackEvent, check Log!");
+            }
         }
     }
 
