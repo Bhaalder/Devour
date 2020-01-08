402beead01fe8fd7dc6559dae6a0036af4874b5a
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2020-01-08 19:52:29 +0100

    Update EndGameEssenceTouch.cs

--- Devour/Devour/Assets/Scripts/EndGame/EndGameEssenceTouch.cs
+++ Devour/Devour/Assets/Scripts/EndGame/EndGameEssenceTouch.cs
@@ -6,27 +6,38 @@ using UnityEngine;
 public class EndGameEssenceTouch : MonoBehaviour
 {
 
+    private bool hasTouchedEssence;
+
+    private void Start()
+    {
+        hasTouchedEssence = false;  
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (!hasTouchedEssence)
         {
-            AudioPlaySoundEvent voidEssenceGainSound = new AudioPlaySoundEvent
+            if (collision.gameObject.tag == "Player")
             {
-                name = "GainVoidEssence",
-                isRandomPitch = false,
-                soundType = SoundType.SFX
-            };
-            PlayerBusyEvent playerBusy = new PlayerBusyEvent
-            {
-                playerIsBusy = true
-            };
-            PlayerTookLastEssenceEvent endGame = new PlayerTookLastEssenceEvent { };
-            playerBusy.FireEvent();
-            voidEssenceGainSound.FireEvent();
-            endGame.FireEvent();
-            GameController.Instance.Player.transform.position = new Vector3(0, -5000, 0);
-
-            Destroy(gameObject);
+                hasTouchedEssence = true;
+                AudioPlaySoundEvent voidEssenceGainSound = new AudioPlaySoundEvent
+                {
+                    name = "GainVoidEssence",
+                    isRandomPitch = false,
+                    soundType = SoundType.SFX
+                };
+                PlayerBusyEvent playerBusy = new PlayerBusyEvent
+                {
+                    playerIsBusy = true
+                };
+                PlayerTookLastEssenceEvent endGame = new PlayerTookLastEssenceEvent { };
+                playerBusy.FireEvent();
+                voidEssenceGainSound.FireEvent();
+                endGame.FireEvent();
+                GameController.Instance.Player.transform.position = new Vector3(0, -5000, 0);
+                Destroy(gameObject);
+            }
         }
+
     }
 }
